f42fcf9d045e047b85e255222c2d39799a9f9f94
Faiz Saiyad <fsaiyad990@gmail.com>
2023-09-18 23:52:39 +0530

    initial commit;

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spacers
+
+Package to add consistent spacing in between widgets in application
---
+++ lib/spacers.dart
@@ -0,0 +1,15 @@
+library spacers;
+
+import 'package:flutter/widgets.dart';
+
+addVerticalSpace(double height) {
+  return SizedBox(
+    height: height,
+  );
+}
+
+addHorizontalSpace(double width) {
+  return SizedBox(
+    width: width,
+  );
+}
